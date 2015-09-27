777bb36317681aaee57af06605d44695d651550e
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-09-27 22:37:55 +0300

    feature identify method complete

--- bioseq2/code.py
+++ bioseq2/code.py
@@ -1,6 +1,12 @@
 def identify(s1, s2):
     abs_matching = 0
-    for i in range(min(len(s1), len(s2))):
+    len_alignment = min(len(s1), len(s2))
+    alignment = ""
+    for i in range(len_alignment):
         if s1[i] == s2[i]:
             abs_matching += 1
-    return abs_matching
+            alignment += '*'
+        else:
+            alignment += ' '
+    percent = abs_matching * 100.0 / len_alignment
+    return abs_matching, len_alignment, percent, alignment
--- bioseq2/test_code.py
+++ bioseq2/test_code.py
@@ -4,13 +4,19 @@ import code as c
 
 
 class Test(unittest.TestCase):
-    def test_identify(self):
+    def test_simple_identify(self):
         seq1 = 'ALIGNMENTS'
         seq2 = 'ALIGDVENTS'
-        abs_matching = c.identify(seq1, seq2)
-        self.assertEqual(8, abs_matching)
 
+        abs_matching, len_alignment, percent, alignment = c.identify(seq1, seq2)
+
+        self.assertEqual(8, abs_matching)
+        self.assertEqual(len(seq1), len_alignment)
+        self.assertAlmostEqual(80.0, percent, 1)
+        self.assertEqual("****  ****", alignment)
+
+    def test_full_identify(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
-
