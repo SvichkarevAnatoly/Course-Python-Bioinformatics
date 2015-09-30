459b98e24070d81ebd89cd3dba034b00fdc23267
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-10-01 02:47:51 +0300

    test consensus on sample

--- bioseq3/test_code.py
+++ bioseq3/test_code.py
@@ -1,13 +1,25 @@
 import unittest
 
-import code as co
+import code as c
 
 
 class Test(unittest.TestCase):
+    seqs = [
+        "SRPAPVVLIILCVMAGVIGTILLISYGIRLLIK",
+        "TVPAPVVIILIILCVMAGIIGTILLLIISYTIRRLIK",
+        "HHFSEPEITLIIFGVMAGVIGTILLLIISYGIRLIK",
+        "HFSELVIALIIFGVMAGVIGTILFISYGSRLIK"
+    ]
+
     def test_simple_consensus(self):
         seqs = ["AC", "AP"]
-        consensus_str = co.consensus(seqs)
+        consensus_str = c.consensus(seqs)
         self.assertEqual("AP", consensus_str)
+
+    def test_consensus(self):
+        consensus_str = c.consensus(self.seqs)
+        expected_consensus_str = "HXPAPVVIIIIIXXVMAGIIGTILLLIISYLIKRIIK"
+        self.assertEqual(expected_consensus_str, consensus_str)
 
 if __name__ == "__main__":
     unittest.main()
