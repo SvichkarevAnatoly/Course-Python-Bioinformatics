318790f3654a79ed4a7922eb05f7498fd3e95848
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-11-01 02:33:04 +0300

    feature implement task3 with logistic activation function

--- bioseq5/run_big_training_set.py
+++ bioseq5/run_big_training_set.py
@@ -1,24 +1,31 @@
 import sys
 import code5 as c
+from random import Random
 
 
 def read_training_set(file_name):
     with open(file_name, 'r') as ts_file:
         return map(lambda l: tuple(l.strip().split()), ts_file.readlines())
 
-nn = c.NeuralNet()
+
+def train_and_predict(activation_func="th"):
+    rand = Random(0)
+    nn = c.NeuralNet(rand, activation_func)
+
+    sys.stdout.write('Train: ')
+    nn.train(big_training_set, 10)
+    sys.stdout.write('done\n')
+    sys.stdout.write('Predict: ')
+    predictedClass = nn.predict(testSeq)
+    sys.stdout.write('done\n')
+    print("Test prediction: %s" % predictedClass)
+
 
 sys.stdout.write('Read training set: ')
 big_training_set = read_training_set("SecondStructureTrainData.txt")
 sys.stdout.write('done\n')
 
-sys.stdout.write('Train: ')
-nn.train(big_training_set, 10)
-sys.stdout.write('done\n')
-
-sys.stdout.write('Predict: ')
 testSeq = 'DLLSA'
-predictedClass = nn.predict(testSeq)
-sys.stdout.write('done\n')
 
-print("Test prediction: %s" % predictedClass)
+train_and_predict()
+train_and_predict("logistic")
