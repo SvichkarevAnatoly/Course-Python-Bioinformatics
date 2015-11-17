8e1246be2e3521162647e8beccb971c017741e6e
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-11-17 23:15:37 +0300

    feature task 2 and test

--- bioseq7/code7.py
+++ bioseq7/code7.py
@@ -1,3 +1,7 @@
+import math
+import numpy
+
+
 def mean(data):
     return sum(data) / float(len(data))
 
@@ -7,12 +11,10 @@ def mode(data):
 
 
 def median(data):
-    import numpy
     return numpy.median(data)
 
 
 def dispersion(data):
-    import math
     return math.sqrt(standard_deviation(data))
 
 
@@ -21,3 +23,16 @@ def standard_deviation(data):
     mean = sum(data) / n
     diffs = [v - mean for v in data]
     return sum([d ** 2 for d in diffs]) / n
+
+
+def confidence_interval(data, confidence, is_one_sided=True):
+    import numpy
+    import scipy.stats
+    n = len(data)
+    # Unbiased estimate
+    date_std_dev = numpy.std(data, ddof=1)
+    if not is_one_sided:
+        confidence = 0.5 * (1 + confidence)
+    interval = scipy.stats.t(n - 1).ppf(confidence) * date_std_dev / \
+               math.sqrt(n)
+    return mean(data), interval
--- bioseq7/test_code7.py
+++ bioseq7/test_code7.py
@@ -24,6 +24,13 @@ class Test(unittest.TestCase):
         values = [1, 2, 2, 3, 2, 1, 4, 2, 3, 1, 0]
         self.assertAlmostEqual(1.083306, c.dispersion(values), 5)
 
+    def test_task2(self):
+        values = [1.752, 1.818, 1.597, 1.697, 1.644, 1.593, 1.878, 1.648,
+                  1.819, 1.794, 1.745, 1.827]
+        mean, interval = c.confidence_interval(values, 0.95, False)
+        self.assertAlmostEqual(1.734, mean, 3)
+        self.assertAlmostEqual(0.0615, interval, 4)
+
 
 if __name__ == "__main__":
     unittest.main()
