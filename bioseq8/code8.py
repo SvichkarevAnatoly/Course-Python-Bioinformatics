0a4adacb93bd7a16cf34d7c7feb30e71338e1bca
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2015-11-22 21:42:02 +0300

    feature task2 and tests for it

--- bioseq8/code8.py
+++ bioseq8/code8.py
@@ -1,10 +1,48 @@
 import rpy2.robjects as r
+from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
+
+STAP = SignatureTranslatedAnonymousPackage
 
 
 def t_test(x, y, same_variance=False):
     func = r.r['t.test']
     arg_dict = {'var.equal': same_variance}
     result = func(x=r.FloatVector(x), y=r.FloatVector(y), **arg_dict)
-
+    # return t, p_value
     return result[0][0], result[2][0]
 
+
+def mean(x):
+    func = r.r['mean']
+    result = func(x=r.FloatVector(x))
+    return result[0]
+
+
+def mode(x):
+    # define mode function in R
+    func_str = '''mode <- function(x) {
+        ux <- unique(x)
+        ux[which.max(tabulate(match(x, ux)))]
+    }'''
+    # get moda function
+    rf = STAP(func_str, "mode")
+    result = rf.mode(x=r.FloatVector(x))
+    return result[0]
+
+
+def median(x):
+    func = r.r['median']
+    result = func(x=r.FloatVector(x))
+    return result[0]
+
+
+def dispersion(x):
+    func = r.r['var']
+    result = func(x=r.FloatVector(x))
+    return result[0]
+
+
+def standard_deviation(x):
+    func = r.r['sd']
+    result = func(x=r.FloatVector(x))
+    return result[0]
--- bioseq8/test_code8.py
+++ bioseq8/test_code8.py
@@ -2,6 +2,8 @@ import unittest
 
 import code8 as c
 
+sample = [1, 2, 2, 3, 2, 1, 4, 2, 3, 1, 0]
+
 
 class Test(unittest.TestCase):
     def test_task1(self):
@@ -16,6 +18,20 @@ class Test(unittest.TestCase):
         self.assertAlmostEqual(-2.072, t, 3)
         self.assertAlmostEqual(0.0654, p_value, 4)
 
+    def test_task2_mean(self):
+        self.assertAlmostEqual(1.909091, c.mean(sample), 6)
+
+    def test_task2_mode(self):
+        self.assertEqual(2, c.mode(sample))
+
+    def test_task2_median(self):
+        self.assertEqual(2, c.median(sample))
+
+    def test_task2_dispersion(self):
+        self.assertAlmostEqual(1.290909, c.dispersion(sample), 6)
+
+    def test_task2_standard_deviation(self):
+        self.assertAlmostEqual(1.1361818, c.standard_deviation(sample), 6)
 
 
 if __name__ == "__main__":
