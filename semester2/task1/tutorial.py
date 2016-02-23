60a2b6d65fdd2959baf3f0abbe51c78b67e53c01
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-02-23 22:19:52 +0300

    feature change tutorial example for task dataset

--- semester2/task1/tutorial.py
+++ semester2/task1/tutorial.py
@@ -1,30 +1,24 @@
-import numpy as np
+from numpy import random, vstack
 from scipy.cluster.hierarchy import dendrogram, linkage
 
 from matplotlib import pyplot as plt
 
-# generate two clusters: a with 100 points, b with 50:
-np.random.seed(4711)  # for repeatability of this tutorial
-a = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100, ])
-b = np.random.multivariate_normal([0, 20], [[3, 1], [1, 4]], size=[50, ])
-X = np.concatenate((a, b), )
-print X.shape  # 150 samples with 2 dimensions
-plt.scatter(X[:, 0], X[:, 1])
-plt.show()
+spread = 0.12
+means = [(0.0, 0.0), (1.0, 1.0), (1.0, 0.0)]
+sizeDims = (100, 2)
+data = [random.normal(means[0], spread, sizeDims),
+        random.normal(means[1], spread, sizeDims),
+        random.normal(means[2], spread, sizeDims)]
+data = vstack(data)
 
 # generate the linkage matrix
-Z = linkage(X, 'ward')
+Z = linkage(data, 'ward')
 
 print Z[:20]
 
 # calculate full dendrogram
-plt.figure(figsize=(25, 10))
 plt.title('Hierarchical Clustering Dendrogram')
 plt.xlabel('sample index')
 plt.ylabel('distance')
-dendrogram(
-    Z,
-    leaf_rotation=90.,  # rotates the x axis labels
-    leaf_font_size=8.,  # font size for the x axis labels
-)
+dendrogram(Z)
 plt.show()
