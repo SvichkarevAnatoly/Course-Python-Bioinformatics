fdfb9889264e863cfd5f5d20091c9a90dd42966b
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-02-23 23:12:12 +0300

    feature add same seed to random for same result with teacher's example

--- semester2/task1/tutorial.py
+++ semester2/task1/tutorial.py
@@ -3,6 +3,8 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 
 from matplotlib import pyplot as plt
 
+random.seed(0)
+
 spread = 0.12
 means = [(0.0, 0.0), (1.0, 1.0), (1.0, 0.0)]
 sizeDims = (100, 2)
@@ -10,6 +12,8 @@ data = [random.normal(means[0], spread, sizeDims),
         random.normal(means[1], spread, sizeDims),
         random.normal(means[2], spread, sizeDims)]
 data = vstack(data)
+plt.scatter(data[:, 0], data[:, 1])
+plt.show()
 
 # generate the linkage matrix
 Z = linkage(data, 'ward')
