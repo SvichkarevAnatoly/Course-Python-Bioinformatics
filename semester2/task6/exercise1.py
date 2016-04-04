3310b9d8f9a67251c05802a961dad05a0e59b465
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-04-04 22:54:58 +0300

    done exercise 1

---
+++ semester2/task6/exercise1.py
@@ -0,0 +1,61 @@
+import numpy
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import tree
+
+# set seed
+numpy.random.seed(1)
+
+nPointsList = range(100, 1001, 100)
+for nPoints in nPointsList:
+    # x values for plotting
+    xPlot = [(float(i) / float(nPoints) - 0.5) for i in range(nPoints + 1)]
+
+    # x needs to be list of lists.
+    x = [[s] for s in xPlot]
+
+    # y (labels) has random noise added to x-value
+    # Build a simple data set with y = x + random
+    y = [s + numpy.random.normal(scale=0.1) for s in xPlot]
+
+    nrow = len(x)
+
+    # fit trees with several different values for depth and use x-validation to see which works best.
+    depthList = [1, 2, 3, 4, 5, 6, 7]
+
+    xvalMSE = []
+    for iDepth in depthList:
+        oosErrors = 0.0
+        # Define test and training index sets
+        idxTest = [a for a in range(nrow) if a % 2 == 0]
+        idxTrain = [a for a in range(nrow) if a % 2 != 0]
+
+        # Define test and training attribute and label sets
+        xTrain = [x[r] for r in idxTrain]
+        xTest = [x[r] for r in idxTest]
+
+        yTrain = [y[r] for r in idxTrain]
+        yTest = [y[r] for r in idxTest]
+
+        # train tree of appropriate depth and accumulate out of sample (oos) errors
+        treeModel = DecisionTreeRegressor(max_depth=iDepth)
+        treeModel.fit(xTrain, yTrain)
+
+        # save last tree
+        if iDepth == depthList[2]:
+            # draw the tree
+            with open("treeEx1.dot", 'w') as f:
+                f = tree.export_graphviz(treeModel, out_file=f)
+
+        treePrediction = treeModel.predict(xTest)
+        error = [yTest[r] - treePrediction[r] for r in range(len(yTest))]
+
+        # accumulate squared errors
+        oosErrors += sum([e * e for e in error])
+
+        # average the squared errors and accumulate by tree depth
+        mse = oosErrors / (nrow * 0.5)
+        xvalMSE.append(mse)
+
+    print "for n=" + str(nPoints) + \
+          " MSEs=" + "[%s]" % ", ".join("%.3f" % x for x in xvalMSE)
