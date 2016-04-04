e7a07938ad458a037056a9d16a99ae9c1a07d125
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-04-04 13:43:42 +0300

    add main function to module

--- semester2/task5/exercise1.py
+++ semester2/task5/exercise1.py
@@ -1,6 +1,6 @@
 import example as e
 
-colors = [
+train_colors = [
     [1, 0, 0, 'warm'],
     [0, 1, 0, 'cool'],
     [0, 0, 1, 'cool'],
@@ -15,10 +15,11 @@ colors = [
     [1, 0.5, 0.5, 'warm'],
 ]
 
-tree = e.buildtree(colors)
-e.printtree(tree)
-e.drawtree(tree, "ex1_original_tree.jpg")
+if __name__ == '__main__':
+    tree = e.buildtree(train_colors)
+    e.printtree(tree)
+    e.drawtree(tree, "ex1_original_tree.jpg")
 
-e.prune(tree, 0.1)
-e.printtree(tree)
-e.drawtree(tree, "ex1_prune_tree.jpg")
+    e.prune(tree, 0.1)
+    e.printtree(tree)
+    e.drawtree(tree, "ex1_prune_tree.jpg")
