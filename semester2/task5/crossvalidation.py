fa11fcb4956550832d68a27dc45e9b5929d9e213
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-04-04 15:07:26 +0300

    feature add list factors checking and improve by declare divide set method

--- semester2/task5/crossvalidation.py
+++ semester2/task5/crossvalidation.py
@@ -1,28 +1,52 @@
-from random import random
+import random
+import example as e
 
 
-def crossvalidate(algf, data, trials=100, test=0.05):
-    error = 0.0
-    for i in range(trials):
-        trainset, testset = dividedata(data, test)
-        error += testalgorithm(algf, trainset, testset)
-    return error / trials
+def crossvalidate(algf, comparator, data, cvfactors, trials=100):
+    errors = []
+    for factor in cvfactors:
+        error = 0.0
+        for i in range(trials):
+            trainset, testset = dividedata(data, factor)
+            error += testalgorithm(algf, comparator, trainset, testset)
+        errors.append(error / trials)
+    return errors
 
 
-def testalgorithm(algf, trainset, testset):
+def testalgorithm(algf, comparator, trainset, testset):
     error = 0.0
     for row in testset:
-        guess = algf(trainset, row['input'])
-        error += (row['result'] - guess) ** 2
+        guess = algf(trainset, row[:-1])  # row without last item
+        error += comparator(row[-1], guess) ** 2  # row's last item
     return error / len(testset)
 
 
-def dividedata(data, test=0.05):
-    trainset = []
-    testset = []
-    for row in data:
-        if random() < test:
-            testset.append(row)
-        else:
-            trainset.append(row)
+def dividedata(data, factor=0.95):
+    testsize = len(data) * (1 - factor)
+    testsize = 1 if testsize < 1 else testsize
+
+    testset = [data[i] for i in random.sample(xrange(len(data)), testsize)]
+    trainset = [item for item in data if item not in testset]
     return trainset, testset
+
+
+def algf_tree(trainset, observation):
+    tree = e.buildtree(trainset)
+    return e.classify(observation, tree)
+
+
+def entropy_on_uniquecounts(uniquecounts_dict):
+    from math import log
+    log2 = lambda x: log(x) / log(2)
+    # Now calculate the entropy
+    ent = 0.0
+    for r in uniquecounts_dict.keys():
+        p = float(uniquecounts_dict[r]) / sum(uniquecounts_dict.values())
+        ent -= p * log2(p)
+    return ent
+
+
+def tree_comparator(test_class, tree_branch, scoref=entropy_on_uniquecounts):
+    if test_class in tree_branch:
+        tree_branch[test_class] -= 1
+    return scoref(tree_branch)
