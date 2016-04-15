5d91684ebd778cba8c4bc8c789aae50293894267
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-04-15 16:01:09 +0300

    done both exercises

--- semester2/task10/exercise2.py
+++ semester2/task10/exercise2.py
@@ -3,7 +3,7 @@ import numpy
 import matplotlib.pyplot as plot
 from sklearn import ensemble
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import confusion_matrix
 
 data = open('glass.data.csv')
 
@@ -42,11 +42,10 @@ for l in labels:
     index = labelList.index(l)
     newLabels.append(index)
 
-# Drawing 30% test sample may not preserve population proportions
-# stratified sampling by labels.
 xTemp = [xNum[i] for i in range(nrows) if newLabels[i] == 0]
 yTemp = [newLabels[i] for i in range(nrows) if newLabels[i] == 0]
-xTrain, xTest, yTrain, yTest = train_test_split(xTemp, yTemp, test_size=0.30, random_state=531)
+xTrain, xTest, yTrain, yTest = train_test_split(xTemp, yTemp,
+                                                test_size=0.30, random_state=531)
 for iLabel in range(1, len(labelList)):
     # segregate x and y according to labels
     xTemp = [xNum[i] for i in range(nrows) if newLabels[i] == iLabel]
@@ -63,42 +62,61 @@ for iLabel in range(1, len(labelList)):
     yTrain = numpy.append(yTrain, yTrainTemp, axis=0)
     yTest = numpy.append(yTest, yTestTemp, axis=0)
 
-missCLassError = []
-nTreeList = range(50, 2000, 50)
+# instantiate model
+nEst = 500
 depth = 5
-for iTrees in nTreeList:
-    maxFeat = 4  # try tweaking
-    glassRFModel = ensemble.RandomForestClassifier(n_estimators=iTrees,
-                                                   max_depth=depth, max_features=maxFeat,
-                                                   oob_score=False, random_state=531)
+learnRate = 0.003
+maxFeatures = 3
+subSamp = 0.5
+glassGBMModel = ensemble.GradientBoostingClassifier(n_estimators=nEst,
+                                                    max_depth=depth, learning_rate=learnRate,
+                                                    max_features=maxFeatures, subsample=subSamp)
 
-    glassRFModel.fit(xTrain, yTrain)
-    # Accumulate auc on test set
-    prediction = glassRFModel.predict(xTest)
-    correct = accuracy_score(yTest, prediction)
+# train
+glassGBMModel.fit(xTrain, yTrain)
 
-    missCLassError.append(1.0 - correct)
+# compute auc on test set as function of ensemble size
+missClassError = []
+missClassBest = 1.0
+predictions = glassGBMModel.staged_decision_function(xTest)
+for p in predictions:
+    missClass = 0
+    for i in range(len(p)):
+        listP = p[i].tolist()
+        if listP.index(max(listP)) != yTest[i]:
+            missClass += 1
+    missClass = float(missClass) / len(p)
 
-print "Missclassification Error for max_depth = " + str(depth)
-print(missCLassError[-1])
+    missClassError.append(missClass)
 
-# generate confusion matrix
-pList = prediction.tolist()
-confusionMat = confusion_matrix(yTest, pList)
-print('')
-print("Confusion Matrix")
-print(confusionMat)
+    # capture best predictions
+    if missClass < missClassBest:
+        missClassBest = missClass
+        pBest = p
 
-# plot training and test errors vs number of trees in ensemble
-plot.plot(nTreeList, missCLassError)
+idxBest = missClassError.index(min(missClassError))
+
+# print best values
+print("Best Missclassification Error for max_depth = " + str(depth))
+print(missClassBest)
+print("Number of Trees for Best Missclassification Error")
+print(idxBest)
+
+# plot training deviance and test auc's vs number of trees in ensemble
+missClassError = [100 * mce for mce in missClassError]
+plot.figure()
+plot.plot(range(1, nEst + 1), glassGBMModel.train_score_,
+          label='Training Set Deviance', linestyle=":")
+plot.plot(range(1, nEst + 1), missClassError, label='Test Set Error')
+plot.legend(loc='upper right')
 plot.xlabel('Number of Trees in Ensemble')
-plot.ylabel('Missclassification Error Rate')
+plot.ylabel('Deviance / Classification Error')
 # plot.show()
-plot.savefig("mer" + str(depth) + "depth.png")
+plot.savefig("perfGB" + str(depth) + "depth.png")
 plot.close()
 
 # Plot feature importance
-featureImportance = glassRFModel.feature_importances_
+featureImportance = glassGBMModel.feature_importances_
 
 # normalize by max importance
 featureImportance = featureImportance / featureImportance.max()
@@ -110,4 +128,13 @@ plot.barh(barPos, featureImportance[idxSorted], align='center')
 plot.yticks(barPos, glassNames[idxSorted])
 plot.xlabel('Variable Importance')
 # plot.show()
-plot.savefig("varImp" + str(depth) + "depth.png")
+plot.savefig("varImpBG" + str(depth) + "depth.png")
+plot.close()
+
+# generate confusion matrix for best prediction.
+pBestList = pBest.tolist()
+bestPrediction = [r.index(max(r)) for r in pBestList]
+confusionMat = confusion_matrix(yTest, bestPrediction)
+print('')
+print("Confusion Matrix")
+print(confusionMat)
